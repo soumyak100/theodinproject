26ec6ad969205ab0d89e0b5a11b71c573dda65a2
soumyak100 <soumyak100@outlook.com>
2026-04-12 12:16:06 +0530

    Add logic to play rock-paper-scissors for 5 rounds
    This adds a new function playGame() to play a complete
    game of 5 rounds. Still this a console version so
    the previous instructions to play the play the
    game apply here.

--- foundations/rock_paper_scissors/script.js
+++ foundations/rock_paper_scissors/script.js
@@ -16,12 +16,94 @@ function getHumanChoice() {
 }
 
 function capitalizeFirstLetter(val) {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1).toLowerCase();
 }
 
+function printMessage(result, humanChoice, computerChoice) {
+	if (result > 0) {
+		console.log("You win! " + capitalizeFirstLetter(humanChoice) + " beats " + capitalizeFirstLetter(computerChoice));
+	} else if (result < 0) {
+		console.log("You lose! " + capitalizeFirstLetter(computerChoice) + " beats " + capitalizeFirstLetter(humanChoice));
+	} else {
+		console.log("It's a draw!");
+	}
+}
+
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
+	let humanChoice = "";
+	let computerChoice = "";
+	let result = 0;
+	// Round 1
+	humanChoice = getHumanChoice();
+	computerChoice = getComputerChoice();
+	result = playRound(humanChoice, computerChoice);
+	if (result < 0) {
+		computerScore += 1;
+	} else if (result > 0) {
+		humanScore += 1;
+	}	
+	printMessage(result, humanChoice, computerChoice);
+
+	//Round 2
+	humanChoice = getHumanChoice();
+	computerChoice = getComputerChoice();
+	result = playRound(humanChoice, computerChoice);
+	if (result < 0) {
+		computerScore += 1;
+	} else if (result > 0) {
+		humanScore += 1;
+	}
+	printMessage(result, humanChoice, computerChoice);
+
+	//Round 3
+	humanChoice = getHumanChoice();
+	computerChoice = getComputerChoice();
+	result = playRound(humanChoice, computerChoice);
+	if (result < 0) {
+		computerScore += 1;
+	} else if (result > 0) {
+		humanScore += 1;
+	}
+	printMessage(result, humanChoice, computerChoice);
+
+	//Round 4
+	humanChoice = getHumanChoice();
+	computerChoice = getComputerChoice();
+	result = playRound(humanChoice, computerChoice);
+	if (result < 0) {
+		computerScore += 1;
+	} else if (result > 0) {
+		humanScore += 1;
+	}
+	printMessage(result, humanChoice, computerChoice);
+
+	//Round 5
+	humanChoice = getHumanChoice();
+	computerChoice = getComputerChoice();
+	result = playRound(humanChoice, computerChoice);
+	if (result < 0) {
+		computerScore += 1;
+	} else if (result > 0) {
+		humanScore += 1;
+	}
+	printMessage(result, humanChoice, computerChoice);
+
+	//Print final message
+	if (humanScore > computerScore) {
+		console.log("You win!");
+	} else if (humanScore < computerScore) {
+		console.log("You lose!");
+	} else {
+		console.log("It is a draw!");
+	}
+}
+
+
 function playRound(humanChoice, computerChoice) {
-	humanScore = 0;
-	computerScore = 0;
+	let humanScore = 0;
+	let computerScore = 0;
 	let hChoice = humanChoice.toLowerCase();
 	let cChoice = computerChoice.toLowerCase();
 	if (hChoice === "rock") {
@@ -43,18 +125,15 @@ function playRound(humanChoice, computerChoice) {
 			computerScore += 1;
 		}
 	}
-	
+
 	if (humanScore > computerScore) {
-		console.log("You win! " + capitalizeFirstLetter(hChoice) + " beats " + capitalizeFirstLetter(cChoice));
+		return 1;
 	} else if (computerScore > humanScore) {
-		console.log("You win! " + capitalizeFirstLetter(cChoice) + " beats " + capitalizeFirstLetter(hChoice));
-	} else {
-		console.log("It's a draw!");
+		return -1;
 	}
+
+	return 0;
 }
 
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
